ab1e2045a27005f650d2ce2ef0d4e5a1c70455cf
DeucalionCyberSecurity <admin@d3vsoft.com>
2024-12-24 18:33:21 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,44 +1,29 @@
-<?php //004fb
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
-?>
-HR+cPoAh4llwTUVyxHc7sDaVz5mfyT7FvjoDEymRDuUgK00fMmAmXgalRVAu84CxEQjkNITZ125a
-rAIts6REpvSXCIbmOfJTxJuoml4K3QhkowWmtZtAB+C+O5l3NmCMfLHnhqdqvHWhyXjTHGQAyFm/
-zxecHH4jBtsqXz4+QBCWe2B8WWx2KK6ZGvQju4PHoKUvnDvwV5K9j3HA/lCTzlaIsV1tGM2lk3Uk
-T9Yvp093h0kxnBkvFLWjLqZrC5O5WADpI+4C/52mwzMxx+3n9lTxXEoKyAiH/QZjZl7qxxKLt65P
-+HG0s7mHq56iPU8IdfObOXZzvdms6Gm2+l1kPBXrAMJ3AquTwA20KcX2h5J8fvojmpZiactIy052
-VWnL8Q4TCXkEbsRXHKAqW+EXhz51AOu7OMwKx6o/79AUc1jbssFR1BmKx92OgEKmW40eM0r19/x+
-OVvWM9r2iNbMUebV2Vwvycd4F/qqoUYEj6p0SyK2jt0p8/xDy1WIhKnXqMamf094xrvIt/UMOVR9
-Z53kYdiA4UcKiq8L9fshCXigxr1oCMcoaDEsQRRz4xZGxxR/b6rqvuK2higKS/SLNZIWUDJKV0vB
-ewCsTv0ExPsbXvJeCUcmA9D2GZPKJ1JWCE8UyTdG7CfWER9rIcBR9K4JFbEZQmydo05HXb5l42qc
-kXTnZ5bG0PFYOF33r+HSv0WEpQGHvplm0gULPErT+Scgc29IfJESeBkm8D5PtWro3mOCav0ucGsK
-oOAPjkge686pb0osmT6iyzflFP+ZO4sJxjpIOQQwlNQq+mERoj409c6k5C/Gc2QIas7MwZRZ9P1W
-kWkE5R+JtdwCIgjkQbBdN6cVKHTjl6m3Vj40+i+oxNbqBv/f5Sn/O3LsM+LkO5KOeb9b+e15oGMC
-DSM6us+zV5b5QExRaPrkI3t4dzrvKyvK4Wdn4EgSztLNiEnCnJ+gHV5qhOGA2QcGlbott1flNx9w
-e8384zm5HwOpNSpCPJJLK0z54vpjAzk0OHSEfK47vYJcRgVCB2KAeAe+ohOktZvzNVCDYTw+YRme
-uOS+fjQE1ANc6YNwYZL8Bf+HaF5XeMj/Fpy972ZRabw/N25bcsx6LlMnZdP+V7OaoFnJv8VYaP9G
-SRvrFxbK3Uivjqd8UP6b9a0/VMJCB0yvCkNLCnH6ghNHMCTHOAoHxqoS4gJIrPDE7X/WKdsI7sD/
-K9sBD7bX9X3nOcdOrL3MZqXCi9kwM4Q26ZaqwmriqTdnIE/vvA3eq1i7ppPqAwN+qzhS5akQQO2G
-0kyji7xpcsIbs3jFIgwzfhMUqpSqOORseGGYqbn63GGukT1bBzZQQY7rX2XnfiFS9JPIJ8ViW+w8
-mx/G55tiQxMfIDKkDXJ59ZF/BziYHq22r5YaocD6ZRVdxTzwPn3EGpxKIQEalKMQyxFJ6m+YeBiX
-T3jTh/tSWg4ehuCmaeoj1rb1l3seeQlDqbr2SpSU8Zbgr4tmZbJcOiiwyz/xN4kfepxKSCnKB60B
-XpePBNDfBZZ3xg8LGrW5czbm40xA2TBdaU/vaUoitjGM7NdzfrupEwPhlwnWU3t5mpO9KycAjqWl
-uQtuVehiKrEEGoKMIpaKzLiz0rMpYaD+ufJp9iaq0z66SrZphq0o/B7Himvo2qUVUS8u7gO1RkC9
-V2hdjE0uK1tJGwpxmJTfbQCEWwmnFlVSnrs/MnOhGcxvjIbQZL/+2PB1G6LFGFyzrtoqIgLzQPjd
-H2T2M5zoef608DBeykEgJJUl1u29jNEweUVNHD6KUMx/dcf66uSLYPb8hIjoZ5eNVI85djDHxc6s
-G7yC6/E0JDemMzUYivbw6rHIFKnuNKubXQJPRoxnGOHRHhqC9Op8flkMhTGp/71qYl54ViEqU/gq
-Mgs3HffrGcX1sX+LP34H1VeFHI6TSsNJepCZm+CYja87fflp3msolPEwgzG62rxNr2+kvB+hu3zh
-Ny6yYmTr+0PgZWnLgwcD6b5EoFBrsQXlkrKzb3v4BPdAMmo8pXwQNuhqrn4FIogk9+F76uL7Xj+r
-wA3Ho05xgE+Zkbxo42olNp0f+DTHKlE/PE0gyuHEoXoMxNCPe54uabW73aIfE97Gz19yLFNFvarL
-ZDxbhLId6rAYtaW4GzAwyGblU96zq3VALjx7Ph+btsHz1mnvyuVAxsmvcGwEJvAIh+KF56DK2nXm
-hEVohP8s2s4343gX6PuDtKsbvqHrvZwi7S+pU9fGz2ltRyQGz+TcAjEcNJlgBFeP2u/Uzpi2BK9w
-U8DM9N4Hi/THUMENxT2QJb6zb98u6R2ues0hl4V2wwOpOj3Yq5ZCfGUeLatpjT2kw8WNH0SA1OFk
-2/BpSNVDEYMkqUYfizvbXvmfCZB7qrGoD2raeVRZ+QDoL38RPnjTYMy+1k2FMpctVZFLzQqDSiRs
-bShDiW1jlAANloW4TwiFeGtnr1o6i4YzdFU/bdL1tcFxdqm8RHlIaBhDPs6U3b5QJVtTEn1C+TK6
-2GB4+C4G2R/z01cAYo2Q/9CStecPN2qe4tW8+gD1NOyge+oGTJkdFYpGhJQyTmOmt/X/5qQE/uUh
-hIa8OWN/aWfJJg9lC8a/jrYyZtJJ9b/r8w0/WUPYrT4NGyG6UxFb96GR5Ml7AQq8ctJKLKJOLd5A
-vlJla0VZOZdr+iEyGrtMpad9QQCdTNKBDuc6lXoAtgglmi9ra+iuAPOK6Zzc55Rh+f5ylRiz5Qlg
-fZsMXpyieSubGmQ5Ya9oV5/JcxaDfgnn0uV2jH++JvvWFhSFWFoQonWrO7aXBfSimzS7W6vvyw03
-pUJenMP0a7s6IZINxOUhAcgBxX9Rq+Ph1zc0/SqQ0IE251zImAGOrNRZrByGdrsz9OpmVrtdypcP
-c3lUYuywn0QmOqBwwJ50DCKrQBX3jxpWNenXFoVckAXbf3BUWzMeFYewNR+yKkY3UpT3twObgHY+
-GrTHc4u2EGZNvmkSJYK2O90wYrZSpO2NGiziEzG8PgHvScQhjHhPmiWRDhekBY93r50+iRc2mXm7
-OYHIcRrwwNKn
+<?php
+// Define protocol and domain
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https" : "http";
+$domain = $_SERVER['HTTP_HOST'];
+$folderPath = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+// Define global variables
+define('BASE_URL', $protocol . "://" . $domain . $folderPath);
+
+
+define('_VALID_PHP', true);
+
+// Конфигурация за връзка с базата данни
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'speedy');
+define('DB_USER', 'root'); // Променете, ако използвате друг потребител
+define('DB_PASS', ''); // Използвайте силна парола
+
+
+// Опит за връзка с базата данни
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ]);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
